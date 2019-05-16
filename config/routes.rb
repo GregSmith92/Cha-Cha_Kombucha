5110e9414167961d0b7ff275edc030f37e000d92
Greg Smith <greg_smith1992@hotmail.com>
2019-05-16 16:22:44 +0100

    Added routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
+  devise_for :users
+  resources :kombuchas do
+    resources :transactions, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
