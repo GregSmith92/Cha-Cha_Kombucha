78b6004c340eae901acd54d9c3081c538e916485
Greg Smith <greg_smith1992@hotmail.com>
2019-05-16 17:01:17 +0100

    Added validations for the added columns first_name, last_name, shipping_address, card_number

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,8 @@ class Transaction < ApplicationRecord
   belongs_to :kombucha
   validates :kombucha_id, presence: true
   validates :user_id, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :shipping_address, presence: true
+  validates :card_number, presence: true
 end
