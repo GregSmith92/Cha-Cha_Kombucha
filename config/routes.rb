04f8859b81ba776fce7f951867a11bc8e3dc8f8a
Greg Smith <greg_smith1992@hotmail.com>
2019-05-17 16:14:40 +0100

    Changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
-  resources :kombuchas
-  get 'all', to: 'kombuchas#all_kombuchas' do
+  resources :kombuchas do
+    member do
+      get 'all_kombuchas', to: 'kombuchas#all_kombuchas', as: :all
+    end
     resources :transactions, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
