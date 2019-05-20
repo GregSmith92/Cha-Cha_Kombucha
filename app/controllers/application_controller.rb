efce256361bfc3750abaf4621c984b402c7ae7ff
Greg Smith <greg_smith1992@hotmail.com>
2019-05-20 12:18:36 +0100

    Ran migration update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,10 +19,10 @@ before_action :authenticate_user!
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_pic])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_pic, :first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_pic])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_pic, :first_name, :last_name])
   end
 
   def skip_pundit?
